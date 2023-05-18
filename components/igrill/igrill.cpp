5ba2c69cbbb25d0e4b0e49ca9e492b80b75c4643
Bendik Wang Andreassen <bendik.andreassen@gmail.com>
2023-05-18 18:56:03 +0200

    Renames variable

--- components/igrill/igrill.cpp
+++ components/igrill/igrill.cpp
@@ -270,11 +270,11 @@ namespace esphome
         this->unit_of_measurement_ = CELCIUS_UNIT_STRING;
       }
       ESP_LOGI(TAG, "Setting temperature unit based on device: %s", this->unit_of_measurement_);
-      for (auto & element : this->sensors_)
+      for (auto & sensor : this->sensors_)
       {
-        if (element)
+        if (sensor)
         {
-          element->set_unit_of_measurement(this->unit_of_measurement_);
+          sensor->set_unit_of_measurement(this->unit_of_measurement_);
         }
       }
       request_read_values_();
